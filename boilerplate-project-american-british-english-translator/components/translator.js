8c0037ab72dd5a36162dceb384760f40b4708b4b
phoenix351 <kuat.ponimin123@gmail.com>
2025-01-09 13:40:06 +0800

    add translator challange

---
+++ boilerplate-project-american-british-english-translator/components/translator.js
@@ -0,0 +1,15 @@
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
+
+class Translator {
+  constructor() {
+    this.americanOnly = americanOnly;
+    this.americanToBritishSpelling = americanToBritishSpelling;
+    this.americanToBritishTitles = americanToBritishTitles;
+    this.britishOnly = britishOnly;
+  }
+}
+
+module.exports = Translator;
